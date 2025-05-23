4e1fcde5c9d08637ee0f4799e41ec3f0052e0925
GabPGomes <gabpgomes@gmail.com>
2025-05-23 15:24:08 -0300

    modifiquei para o blink

--- led_counter.v
+++ led_counter.v
@@ -1,31 +1,28 @@
-module Counter #(
+module Blink #(
     parameter CLK_FREQ = 25_000_000 
 ) (
-    input  wire clk,
-    input  wire rst_n,
-    output wire [7:0] leds
+    input wire clk,
+    input wire rst_n,
+    output reg [7:0] leds
 );
 
 localparam ONE_SECOND  = CLK_FREQ;
 localparam HALF_SECOND = CLK_FREQ / 2;
 
-reg [32:0] counter;
-reg  [7:0] led_counter;
+reg [31:0] counter;
 
 always @(posedge clk ) begin
-    if(!rst_n) begin
-        counter     <= 32'h0;
-        led_counter <= 8'h0;
+    if (!rst_n) begin
+        counter <= 32'h0;
+        leds     <= 8'b0;
     end else begin
-        if(counter >= HALF_SECOND - 1) begin
-            counter     <= 32'h0;
-            led_counter <= led_counter + 1'b1;
+        if (counter >= HALF_SECOND - 1) begin
+            counter <= 32'h0;
+            leds[0]     <= ~leds[0];
         end else begin
-            counter <= counter + 1'b1;
+            counter <= counter + 1;
         end
     end
 end
-
-assign leds = led_counter;
     
 endmodule
